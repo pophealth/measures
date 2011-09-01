1ee9701e9ed9d024a317f53131cd859f46ad1c19
Marc Hadley <mhadley@mitre.org>
2011-09-01 13:43:15 -0400

    Fix double counting issue where the same medication could be counted more than once

--- js/asthma_common.js
+++ js/asthma_common.js
@@ -7,55 +7,73 @@ function () {
   var root = this;
   
   root.asthmaNumerator = function(measure, earliest_encounter, effective_date) {
-    var antiasmathic_med = 
-      inRange(measure.antiasthmatic_combinations_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.antiasthmatic_combinations_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.antiasthmatic_combinations_medication_dispensed, earliest_encounter, effective_date) ;
+    var antiasmathic_med_list = _.uniq(normalize(
+      measure.antiasthmatic_combinations_medication_active,
+      measure.antiasthmatic_combinations_medication_order,
+      measure.antiasthmatic_combinations_medication_dispensed
+    ));
+    var antiasmathic_med = inRange(antiasmathic_med_list, earliest_encounter, effective_date);
    
-    var antibody_med = 
-      inRange(measure.antibody_inhibitor_medication_active, earliest_encounter, effective_date)+
-      inRange(measure.antibody_inhibitor_medication_order, earliest_encounter, effective_date)+
-      inRange(measure.antibody_inhibitor_medication_dispensed, earliest_encounter, effective_date) ;
+    var antibody_med_list = _.uniq(normalize(
+      measure.antibody_inhibitor_medication_active,
+      measure.antibody_inhibitor_medication_order,
+      measure.antibody_inhibitor_medication_dispensed
+    ));
+    var antibody_med = inRange(antibody_med_list, earliest_encounter, effective_date);
   
-    var corticosteroid_med = 
-      inRange(measure.inhaled_corticosteroids_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.inhaled_corticosteroids_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.inhaled_corticosteroids_medication_dispensed, earliest_encounter, effective_date);
+    var corticosteroid_med_list = _.uniq(normalize(
+      measure.inhaled_corticosteroids_medication_active,
+      measure.inhaled_corticosteroids_medication_order,
+      measure.inhaled_corticosteroids_medication_dispensed
+    ));
+    var corticosteroid_med = inRange(corticosteroid_med_list, earliest_encounter, effective_date);
       
-    var steroid_med = 
-      inRange(measure.inhaled_steroid_combinations_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.inhaled_steroid_combinations_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.inhaled_steroid_combinations_medication_dispensed, earliest_encounter, effective_date);
+    var steroid_med_list = _.uniq(normalize(
+      measure.inhaled_steroid_combinations_medication_active,
+      measure.inhaled_steroid_combinations_medication_order,
+      measure.inhaled_steroid_combinations_medication_dispensed
+    ));
+    var steroid_med = inRange(steroid_med_list, earliest_encounter, effective_date);
   
-    var mast_cell_med = 
-      inRange(measure.mast_cell_stabilizer_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.mast_cell_stabilizer_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.mast_cell_stabilizer_medication_dispensed, earliest_encounter, effective_date);
+    var mast_cell_med_list = _.uniq(normalize(
+      measure.mast_cell_stabilizer_medication_active,
+      measure.mast_cell_stabilizer_medication_order,
+      measure.mast_cell_stabilizer_medication_dispensed
+    ));
+    var mast_cell_med = inRange(mast_cell_med_list, earliest_encounter, effective_date);
   
-    var methylxanthine_med = 
-      inRange(measure.methylxanthines_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.methylxanthines_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.methylxanthines_medication_dispensed, earliest_encounter, effective_date);
+    var methylxanthine_med_list = _.uniq(normalize(
+      measure.methylxanthines_medication_active,
+      measure.methylxanthines_medication_order,
+      measure.methylxanthines_medication_dispensed
+    ));
+    var methylxanthine_med = inRange(methylxanthine_med_list, earliest_encounter, effective_date);
   
     return (antiasmathic_med + antibody_med + corticosteroid_med + steroid_med  +
       mast_cell_med + methylxanthine_med); 
   }
   
   root.asthmaDenominator = function(measure, earliest_encounter, effective_date) {
-    var long_acting_beta_med = 
-      inRange(measure.long_acting_inhaled_beta_2_agonist_medication_active, earliest_encounter, effective_date)+
-      inRange(measure.long_acting_inhaled_beta_2_agonist_medication_order, earliest_encounter, effective_date)+
-      inRange(measure.long_acting_inhaled_beta_2_agonist_medication_dispensed, earliest_encounter, effective_date);
+    var long_acting_beta_med_list = _.uniq(normalize(
+      measure.long_acting_inhaled_beta_2_agonist_medication_active,
+      measure.long_acting_inhaled_beta_2_agonist_medication_order,
+      measure.long_acting_inhaled_beta_2_agonist_medication_dispensed
+    ));
+    var long_acting_beta_med = inRange(long_acting_beta_med_list, earliest_encounter, effective_date);
   
-    var short_acting_beta_med = 
-      inRange(measure.short_acting_beta_2_agonist_medication_active, earliest_encounter, effective_date)+
-      inRange(measure.short_acting_beta_2_agonist_medication_order, earliest_encounter, effective_date)+
-      inRange(measure.short_acting_beta_2_agonist_medication_dispensed, earliest_encounter, effective_date);
+    var short_acting_beta_med_list = _.uniq(normalize(
+      measure.short_acting_beta_2_agonist_medication_active,
+      measure.short_acting_beta_2_agonist_medication_order,
+      measure.short_acting_beta_2_agonist_medication_dispensed
+    ));
+    var short_acting_beta_med = inRange(short_acting_beta_med_list, earliest_encounter, effective_date);
 
-    var leukotriene_med = 
-      inRange(measure.leukotriene_inhibitors_medication_active, earliest_encounter, effective_date) +
-      inRange(measure.leukotriene_inhibitors_medication_order, earliest_encounter, effective_date) +
-      inRange(measure.leukotriene_inhibitors_medication_dispensed, earliest_encounter, effective_date);
+    var leukotriene_med_list = _.uniq(normalize(
+      measure.leukotriene_inhibitors_medication_active,
+      measure.leukotriene_inhibitors_medication_order,
+      measure.leukotriene_inhibitors_medication_dispensed
+    ));
+    var leukotriene_med = inRange(leukotriene_med_list, earliest_encounter, effective_date);
   
     var denom_meds = long_acting_beta_med + short_acting_beta_med + asthmaNumerator(measure, earliest_encounter, effective_date);
     var ed_encounter = inRange(measure.encounter_ed_encounter, earliest_encounter, effective_date);
