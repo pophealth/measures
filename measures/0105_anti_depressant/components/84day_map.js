8caf15d172ea3a1e38306b1291fe6f952409612f
Jason Letourneau <jletourneau80@gmail.com>
2012-06-26 17:49:51 -0400

    fixed typo in 0105 for ordered vs order and added patch to 0073 to fix the code selection problem (diastolic vs systolic)

--- measures/0105_anti_depressant/components/84day_map.js
+++ measures/0105_anti_depressant/components/84day_map.js
@@ -38,15 +38,15 @@ function() {
                                                                   all_encounters,
                                                                   earliest_encounter,
                                                                   latest_encounter);
-    if (depression_during_encounter.length <= 1 && depression_principal_during_encounter == 0) {
+    if (depression_during_encounter.length <= 1 && depression_principal_during_encounter.length == 0) {
       return(false);
     }
 
     first_diagnosis = _.min(depression_during_encounter);
     var meds_dispensed_before_first_diagnosis30 = inRange(measure.antidepressant_medications_medication_dispensed,first_diagnosis - (30 * day), first_diagnosis);
     var meds_dispensed_after_first_diagnosis14 = inRange(measure.antidepressant_medications_medication_dispensed, first_diagnosis, first_diagnosis + (14 * day));
-    var meds_ordered_before_first_diagnosis30 = inRange(measure.antidepressant_medications_medication_ordered,first_diagnosis - (30 * day), first_diagnosis);
-    var meds_ordered_after_first_diagnosis14 = inRange(measure.antidepressant_medications_medication_ordered, first_diagnosis, first_diagnosis + (14 * day));
+    var meds_ordered_before_first_diagnosis30 = inRange(measure.antidepressant_medications_medication_order,first_diagnosis - (30 * day), first_diagnosis);
+    var meds_ordered_after_first_diagnosis14 = inRange(measure.antidepressant_medications_medication_order, first_diagnosis, first_diagnosis + (14 * day));
     var meds_active_before_first_diagnosis30 = inRange(measure.antidepressant_medications_medication_active,first_diagnosis - (30 * day), first_diagnosis);
     var meds_active_after_first_diagnosis14 = inRange(measure.antidepressant_medications_medication_active, first_diagnosis, first_diagnosis + (14 * day));
 
