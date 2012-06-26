416c76003a552c0207059b80ba42e9a3329ca6db
Ryan C. Chan <rchan@mitre.org>
2012-06-26 12:01:13 -0400

    Move converter

--- converter.js
+++ converter.js
@@ -29,9 +29,40 @@ toObj = function(obj){
   };
 };
 
-var minify = process.argv[2] == '--min'
+var minify = $.inArray('--min', process.argv) > -1;
+var verbose = $.inArray('-v', process.argv) > -1;
 
-fs.readdir('.', function(e, files){
+(convertJSON = function(dir){
+  if (dir.match(/node_modules/)) return;
+  if (verbose) console.log('Descending into ' + dir);
+  fs.readdir(dir, function(e, files){
+    $.each(files, function(i, file){
+      fs.stat(dir + '/' + file, function(e, s) {
+        if (s.isDirectory()){
+          if (verbose) console.log(dir + '/' + file + ' is a directory');
+          convertJSON(dir + '/' + file);
+        }
+        else {
+          if (!file.match(/\.v2\.json$/) && !file.match(/\.v2\.min\.json$/) && file.match(/\.json$/)){
+            if (verbose) console.log('Processsing file: ' + dir+'/'+file);
+            fs.readFile(dir+'/'+file, 'utf8',
+              function(file, e, data){
+                data = JSON.parse(data);
+                data.numerator = data.numerator ? toObj(data.numerator) : undefined;
+                data.denominator = data.denominator ? toObj(data.denominator) : undefined;
+                data.population = data.population ? toObj(data.population) : undefined;
+                data.exclusions = data.exclusions ? toObj(data.exclusions) : undefined;
+                fs.writeFile(dir+'/'+file.match(/(.+)\.json/)[1]+'.v2' + (minify?'.min':'') + '.json', JSON.stringify(data, null, minify ? null : 2), 'utf8');
+              }.curry(file)
+            );
+          }
+        }
+      });
+    });
+  });
+})('measures')
+
+/*fs.readdir('.', function(e, files){
   $.each(files, function(i, dir){
     if (dir.match(/^\d{4}/)){
       fs.readdir(dir,  function(e, files){
@@ -54,3 +85,4 @@ fs.readdir('.', function(e, files){
     }
   });
 });
+*/
